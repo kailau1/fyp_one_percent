76596da1f27b8df05899d9690dc11a967addb2da
kailau1 <kailau112@gmail.com>
2025-01-23 16:58:07 +0000

    Removed redundant code from login.tsx

--- app/auth/login.tsx
+++ app/auth/login.tsx
@@ -1,13 +1,12 @@
 import { StyleSheet,Text, } from 'react-native';
 import { ThemedView } from '@/components/ThemedView';
-import { ThemedText } from '@/components/ThemedText';
 import ButtonCTA from '@/components/ui/ButtonCTA';
 import {useState} from 'react';
-import { MaterialIcons } from '@expo/vector-icons';
 import { useRouter } from 'expo-router';
 import { useColorScheme } from 'react-native';
 import InputField from '@/components/InputField';
 import { loginUser } from '@/scripts/services/userService';
+import  Header  from '@/components/Header';
 
 export default function LoginScreen() {
     const router = useRouter();
@@ -35,22 +34,17 @@ export default function LoginScreen() {
 
     return (
         <ThemedView style={styles.container}>
-            <ThemedView style={styles.headerContainer}>
-                <MaterialIcons name="arrow-back" size={28} color={iconColour} onPress={() => router.back()} style={styles.backIcon}/>
-                <ThemedText style={styles.title}>Enter Your Details</ThemedText> 
-            </ThemedView>
+            <Header title="Enter your details" onBackPress={() => router.back()} iconColour={iconColour} />
             <InputField
                 placeholder="Enter your email"
                 icon="mail"
                 onChangeText={newText => setEmail(newText)}
-
             />
             <InputField
                 placeholder="Password"
                 icon="key"
                 secureTextEntry={true}
                 onChangeText={newText => setPassword(newText)}
-
             />
             <ThemedView style={styles.errorContainer}>
                 {errors.email && <Text style={styles.errorText}>{errors.email}</Text>}
@@ -70,56 +64,6 @@ const styles = StyleSheet.create({
         padding: 10,
         position: 'relative',
         height: '100%',
-    },
-    backIcon: {
-        position: 'absolute',
-        left: 0,
-    },
-    headerContainer: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        width: '100%',
-        justifyContent: 'center',
-        marginTop: '3%',
-    },
-    title: {
-        textAlign: 'center',
-        fontFamily: 'Comfortaa_400Regular',
-        fontWeight: 500,
-        fontSize: 32,
-        marginTop: '14%',
-        marginBottom: '3%',
-        lineHeight: 36,
-    },
-    textFieldsContainer: {
-        alignItems: 'center',
-        padding: 10,
-        position: 'relative',
-        width: '100%',
-        marginTop: '7%'
-    },
-    textFieldLarge: {
-        padding: 10,
-        width: '100%',
-        fontFamily: 'Comfortaa_400Regular',
-        outlineWidth: 0,
-        fontSize: 18
-    },
-    inputContainer: {
-        backgroundColor: '#F0F0F0',
-        position: 'relative', 
-        width: '90%',
-        borderWidth: 1,
-        borderColor: '#ccc',
-        borderRadius: 30,
-        paddingLeft: '8%', 
-        marginTop: '5%',
-    },
-    icon: {
-        position: 'absolute', 
-        left: 10,
-        top: '50%',
-        transform: [{ translateY: -10 }], 
     },
     buttonContainer: {
         alignItems: 'center',
